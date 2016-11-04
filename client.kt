358d8a5bfb1a057a010efec4fd068298545c5e60
spinclick <spinclick@users.noreply.github.com>
2016-11-03 20:04:13 -0400

    Catch ConnectException

--- client.kt
+++ client.kt
@@ -1,6 +1,7 @@
 package client
 
 import java.net.Socket
+import java.net.ConnectException
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 import java.io.BufferedInputStream
@@ -11,7 +12,7 @@ import org.json.simple.JSONArray
 import org.json.simple.JSONValue
 
 val HOST = "127.0.0.1"
-val PORT = 9993
+val PORT = 9944
 
 class AP(ssid: String, bssid: String) {
   public val ssid = ssid
@@ -150,57 +151,62 @@ fun main(args: Array<String>) {
   var appUser = User("id000000","username", 0)
 
   loop@ while (true) {
-    when(getline(">")) {
-      "get" -> println("id = '${appUser.id}', user = '${appUser.name}'")
-      "set id" -> appUser = User(getline("id: "), appUser.name,appUser.lastUpdated)
-      "set user" -> appUser = User(appUser.id, getline("user: "), appUser.lastUpdated)
-      "create" -> {
-        val requestedUser = getline("initial username: ")
-        val createJson = createToJson(requestedUser)
-        println("SEND: ${createJson.toString()}")
-        val createResponse = sendJsonWithResponse(HOST,PORT,createJson)
-        println("RECV: $createResponse")
-        val innerjson = createResponse.get("CreateResponse") as JSONObject
-        val newid = if (innerjson.get("id") == null) "none" else innerjson.get("id") as String
-        println("Updated id ${appUser.id} -> $newid")
-        appUser = User(newid, appUser.name, appUser.lastUpdated)
-      }
-      "query location" -> {
-        val location = getline("location: ")
-        val queryJson = queryLocationToJson(appUser, location)
-        println("SEND: ${queryJson.toString()}")
-        val queryResponse = sendJsonWithResponse(HOST,PORT,queryJson)
-        println("RECV: $queryResponse")
-      }
-      "query users" -> {
-        val numUsers = try {getline("# users: ").toInt()} catch (e: Exception) {0}
-        val users = Array<String>(numUsers, {getline("user: ")})
-        val queryJson = queryUsersToJson(appUser, users)
-        println("SEND: ${queryJson.toString()}")
-        val queryResponse = sendJsonWithResponse(HOST,PORT,queryJson)
-        println("RECV: $queryResponse")
-      }
-      "update aps" -> {
-        val numAps = try {getline("# aps: ").toInt()} catch (e: Exception) {0}
-        val aps = Array<AP>(numAps, {AP(getline("ssid: "), getline("bssid: "))})
-        val apsJson = apsToJson(aps)
-        val updateJson = updateToJson(appUser, apsJson)
-        println("SEND: ${updateJson.toString()}")
-        sendJsonNoResponse(HOST,PORT,updateJson)
-      }
-      "update gps" -> {
-        val gps = try {
-          GPS(getline("lat: ").toDouble(), getline("lon: ").toDouble())
-        } catch(e: Exception){
-          GPS(0.0,0.0)
+    try {
+      when(getline(">")) {
+          "get" -> println("id = '${appUser.id}', user = '${appUser.name}'")
+          "set id" -> appUser = User(getline("id: "), appUser.name,appUser.lastUpdated)
+          "set user" -> appUser = User(appUser.id, getline("user: "), appUser.lastUpdated)
+          "create" -> {
+            val requestedUser = getline("initial username: ")
+            val createJson = createToJson(requestedUser)
+            println("SEND: ${createJson.toString()}")
+            val createResponse = sendJsonWithResponse(HOST,PORT,createJson)
+            println("RECV: $createResponse")
+            val innerjson = createResponse.get("CreateResponse") as JSONObject
+            val newid = if (innerjson.get("id") == null) "none" else innerjson.get("id") as String
+            println("Updated id ${appUser.id} -> $newid")
+            appUser = User(newid, appUser.name, appUser.lastUpdated)
+          }
+          "query location" -> {
+            val location = getline("location: ")
+            val queryJson = queryLocationToJson(appUser, location)
+            println("SEND: ${queryJson.toString()}")
+            val queryResponse = sendJsonWithResponse(HOST,PORT,queryJson)
+            println("RECV: $queryResponse")
+          }
+          "query users" -> {
+            val numUsers = try {getline("# users: ").toInt()} catch (e: Exception) {0}
+            val users = Array<String>(numUsers, {getline("user: ")})
+            val queryJson = queryUsersToJson(appUser, users)
+            println("SEND: ${queryJson.toString()}")
+            val queryResponse = sendJsonWithResponse(HOST,PORT,queryJson)
+            println("RECV: $queryResponse")
+          }
+          "update aps" -> {
+            val numAps = try {getline("# aps: ").toInt()} catch (e: Exception) {0}
+            val aps = Array<AP>(numAps, {AP(getline("ssid: "), getline("bssid: "))})
+            val apsJson = apsToJson(aps)
+            val updateJson = updateToJson(appUser, apsJson)
+            println("SEND: ${updateJson.toString()}")
+            sendJsonNoResponse(HOST,PORT,updateJson)
+          }
+          "update gps" -> {
+            val gps = try {
+              GPS(getline("lat: ").toDouble(), getline("lon: ").toDouble())
+            } catch(e: Exception){
+              GPS(0.0,0.0)
+            }
+            val gpsJson = gpsToJson(gps)
+            val updateJson = updateToJson(appUser,gpsJson)
+            println("SEND: ${updateJson.toString()}")
+            sendJsonNoResponse(HOST,PORT,updateJson)
+          }
+          "q" -> break@loop
+          else -> {printCmds()}
         }
-        val gpsJson = gpsToJson(gps)
-        val updateJson = updateToJson(appUser,gpsJson)
-        println("SEND: ${updateJson.toString()}")
-        sendJsonNoResponse(HOST,PORT,updateJson)
-      }
-      "q" -> break@loop
-      else -> {printCmds()}
+    }
+    catch (e: ConnectException) {
+      println("Could not connect to $HOST on $PORT")
     }
   }
 }
